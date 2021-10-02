57cc52c21faff535a22ab22538912185e64407e7
Max Zhuravsky <paperlark@yandex.com>
2021-10-02 21:23:24 +0300

    [master] - initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tools for Magic the Gathering
+
+Repository contains sources for website which provides various tools useful when playing Magic the Gathering.
---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.5.0"
+}
+
+group = "com.paperlark"
+version = "0.0.1"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
+    mavenCentral()
+    jcenter()
+}
+
+kotlin {
+    js {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+        binaries.executable()
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC")
+
+    implementation("org.jetbrains:kotlin-react:17.0.2-pre.154-kotlin-1.5.0")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.154-kotlin-1.5.0")
+    implementation(npm("react", "17.0.2"))
+    implementation(npm("react-dom", "17.0.2"))
+
+    implementation("org.jetbrains:kotlin-styled:5.2.3-pre.154-kotlin-1.5.0")
+    implementation(npm("styled-components", "~5.2.3"))
+
+}
